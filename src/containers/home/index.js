763319032a4eb47834206332943d65baaba40d17
rajaishwary <rajaishwary@gmail.com>
2019-10-12 23:44:34 +0530

    add structure

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import {View, Text} from 'react-native';
 
 class Home extends React.Component {
   render() {
+    console.log(this.props.nav);
     return (
       <View>
         <Text>{'Home Container'}</Text>
@@ -10,4 +12,9 @@ class Home extends React.Component {
     );
   }
 }
-export default Home;
+
+function mapStateToProps(state) {
+  return { nav: state.navigation };
+}
+
+export default connect(mapStateToProps)(Home);
