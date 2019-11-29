b636fd95670306fbbb18a22070414c84a7f8eb61
rajaishwary <rajaishwary@gmail.com>
2019-11-29 14:32:46 +0530

    added linear grad

--- src/config/index.js
+++ src/config/index.js
@@ -2,11 +2,6 @@ export const BETA_API = "";
 export const LIVE_API = "";
 export const API_URL = !__DEV__ ? BETA_API : LIVE_API;
 
-export const TestStream = {
-  'rtmpUrl': 'rtmp://fms.105.net/live',
-  'rtmpStreamKey': 'rmc1', 
-};
-
 export default {
   allowTextFontScaling: false,
   API_URL,
