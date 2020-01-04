0a25bc622b9c8ae55bb01062f92210cc534a168c
rajaishwary <rajaishwary@gmail.com>
2020-01-04 21:29:19 +0530

    update button component with styles and config

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,11 +1,31 @@
+const buttonTypes = {
+  primary: 'PRIMARY',
+  secondary: 'SECONDARY',
+  outlined: 'OUTLINED',
+};
+
 export const colors = {
-    primary: '#009688',
-    darkPrimary: '#00796B',
-    lightPrimary: '#B2DFDB',
-    primaryText: '#212121',
-    accent: '#FFC107',
-    secondaryText: '#757575',
-    divider: '#BDBDBD',
-    linearGradient: ['#009688', '#00d1be'],
-    disabled: '#f5f5f5',
+  primary: '#009688',
+  darkPrimary: '#00796B',
+  lightPrimary: '#B2DFDB',
+  primaryText: '#212121',
+  accent: '#FFC107',
+  secondaryText: '#757575',
+  divider: '#BDBDBD',
+  linearGradient: ['#009688', '#00d1be'],
+  disabled: '#f5f5f5',
+  button: {
+    [buttonTypes.primary]: {
+      background: '#009688',
+      text: '#FFF',
+    },
+    [buttonTypes.secondary]: {
+      background: '#757575',
+      text: '#000',
+    },
+    [buttonTypes.outlined]: {
+      background: '#FFFFFF',
+      text: '#000',
+    },
+  },
 };
