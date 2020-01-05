0dafbd782eb1a57612b9a81cabb0bf6af68c0a33
rajaishwary <rajaishwary@gmail.com>
2020-01-05 06:26:07 +0530

    add linear gradient to buttons

--- src/components/LGradient.js
+++ src/components/LGradient.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import LinearGradient from 'react-native-linear-gradient';
-import { colors } from 'src/theme/colors';
+import { colors as themeColors } from 'src/theme/colors';
 
 const LGradient = (props) => {
-    const { children, style } = props;
+    const { children, style, colors } = props;
     return (
-        <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 1}} colors={colors.linearGradient} style={[linearGradient, style]}>
+        <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 1}} colors={colors ? colors : themeColors.linearGradient} style={[linearGradient, style]}>
             {children}
         </LinearGradient>
     );
