fc13953a48df6bb7bfebaaa7c46201ee1cd4d4c0
rajaishwary <rajaishwary@gmail.com>
2020-01-20 21:47:31 +0530

    nav list init

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import LGradient from './LGradient';
 import Button from './Button';
 import Card from './Card';
 import Chip from './Chip';
+import NavList from './NavList';
 
 export {
     Text,
     LGradient,
     Button,
     Card,
-    Chip
-}
+    Chip, 
+    NavList
+};
